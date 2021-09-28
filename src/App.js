0198ec409bfd18f527a8c505d9a8a0256e252a8f
Vitor Rocha Tenório <70405007+VRochinhaT@users.noreply.github.com>
2021-09-28 12:27:49 -0300

    url params

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Switch, Route, useParams } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>URL Params</h2>
+
+      <ul>
+        <li>
+          <Link to="/netflix">Netflix</Link>
+        </li>
+        <li>
+          <Link to="/twitter">Twitter</Link>
+        </li>
+        <li>
+          <Link to="/instagram">Instagram</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route path="/:id" children={<Child />} />
+      </Switch>
+    </div>
+  );
+}
+
+function Child() {
+  let { id } = useParams();
+
+  return (
+    <div>
+      <h3>URL param Id: {id}</h3>
     </div>
   );
 }
